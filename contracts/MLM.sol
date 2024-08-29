4294e66dfeb3d34de53f547d1aadfbdd8ac59995
iamanuragsaini <iamanuragsaini0@gmail.com>
2024-08-29 21:06:19 +0530

    feat: add more state variables

--- contracts/MLM.sol
+++ contracts/MLM.sol
@@ -12,6 +12,15 @@ contract MLM {
     uint256 public cycleCount;
     uint256 private usersInCurrentCycle;
 
+    address[] public userAddresses;
+
+    uint256 public lastUserId = 1;
+    uint8 public constant LAST_LEVEL = 12;
+    uint256 public constant BASIC_PRICE = 5 ether;
+    uint8 public constant SPOTS_X3 = 3;
+    uint8 public constant SPOTS_X4 = 6;
+    uint256 public constant REGISTRATION_FEE = 10 ether;
+
     mapping(address => User) internal users;
     mapping(uint256 => address) public idToAddress;
     mapping(address => uint256) public addressToId;
