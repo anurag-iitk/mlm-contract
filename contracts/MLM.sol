69b731711b817eba75338d2688fc7dfd41151793
iamanuragsaini <iamanuragsaini0@gmail.com>
2024-08-29 21:08:18 +0530

    feat: define constructor to with details to store some values in state variables

--- contracts/MLM.sol
+++ contracts/MLM.sol
@@ -34,4 +34,48 @@ contract MLM {
         string referralLink
     );
 
+    constructor() payable {
+        owner = msg.sender;
+        cycleCount = 1;
+        usersInCurrentCycle = 0;
+
+        UserDetails memory ownerDetails = UserDetails({
+            firstName: "",
+            lastName: "",
+            profilePic: "",
+            email: "",
+            passwordHash: bytes32(0)
+        });
+
+        User storage user = users[owner];
+
+        user.details = ownerDetails;
+        user.info.upline = address(0);
+        user.info.id = 0;
+        user.info.referralCount = 0;
+        user.info.cycleCount = 0;
+        user.info.earnings = 0;
+        user.info.partnerLevel = 1;
+        user.info.referrals = new address[](0);
+        user.info.userId = "0";
+        user.info.uplineId = "";
+        user.info.walletAddress = owner;
+        user.info.referralLink = "";
+        user.info.cycle = 1;
+        user.info.isLevelActive[1] = true;
+
+        idToAddress[1] = owner;
+        addressToId[owner] = 1;
+
+        // Initialize level prices
+        levelPrice[1] = BASIC_PRICE;
+        for (uint8 i = 2; i <= 8; i++) {
+            levelPrice[i] = levelPrice[i - 1] * 2;
+        }
+        levelPrice[9] = 1250 ether;
+        levelPrice[10] = 2500 ether;
+        levelPrice[11] = 5000 ether;
+        levelPrice[12] = 9900 ether;
+    }
+
 }
